5f4d5e226483041e3ba5dcb7903d624da1e457ae
Linares <akuruma025@yandex.ru>
2025-10-04 18:25:50 +0300

    create struct for db func

--- internal/repository/postgres/api/api.go
+++ internal/repository/postgres/api/api.go
@@ -1,1 +1,7 @@
 package api
+
+import (
+	models "github.com/Hirogava/ParkingDealer/internal/models/routresponse"
+)
+
+func (manager *Manager) GetCriticalManeuvers(r *models.RouteResponse, w *models.WeatherResponse) (*models.RouteResponse, error) {}
